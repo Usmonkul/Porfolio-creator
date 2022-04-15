9f1f09347da3f5a796d13a772fd50c9cfbe3cede
Usmon <usmonhakimov1999@gmail.com>
2022-04-15 19:40:18 +0900

    add hamburger menu action

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 "use strict";
+const hamBtn = document.querySelector(".menu-btn");
+hamBtn.addEventListener("click", function (e) {
+  e.preventDefault;
+  this.classList.toggle("menu-btn_active");
+});
