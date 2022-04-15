9345ee5b70e9f0a1261e55933e55b420c192396a
Usmon <usmonhakimov1999@gmail.com>
2022-04-15 23:14:39 +0900

    add responsive ness to header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 const hamBtn = document.querySelector(".menu-btn");
+const elNavList = document.getElementById("jsNavList");
 hamBtn.addEventListener("click", function (e) {
   e.preventDefault;
   this.classList.toggle("menu-btn_active");
+  elNavList.classList.toggle("active");
 });
